91b256f6b9be1117307d6bfac1fb165539699d03
João Vitor <joaovitor86@gmail.com>
2025-02-15 12:14:31 -0300

    Bugfix

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
-// Obtém o botão com o ID 'toggleBtn' no popup e adiciona um ouvinte de evento para o clique
-document.getElementById('toggleBtn').addEventListener('click', function() {
-    console.log("Botão de alternar visibilidade clicado!");  // Exibe uma mensagem no console quando o botão é clicado
+let tgBtn = document.getElementById('toggleBtn');
+
+tgBtn.addEventListener('click', function() {
+    console.log("Botão de alternar visibilidade clicado!");
+	
+	// Pega o texto atual do botão
+    let currentText = tgBtn.innerText;
+	
+	// Altera o texto do botão dependendo do texto atual
+    if (currentText === "Ocultar") {
+        tgBtn.innerText = "Reexibir";  // Altera o texto para "Ocultar"
+    } else {
+        tgBtn.innerText = "Ocultar";  // Altera o texto para "Mostrar"
+    }
     
-    // Envia uma mensagem para o script de background solicitando a alternância de visibilidade
     chrome.runtime.sendMessage({ action: "toggleVisibility" }, function(response) {
-        // Exibe a resposta recebida do script de background no console
         console.log("Resposta recebida do background:", response);
     });
 });
